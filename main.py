8d4b823ffff4ecc79037a9547fb1e625de964839
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-05 16:45:34 -0800

    Implement search options to get temperatures by date  or station

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]]
 
 
+def get_dataframe(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    return df
+
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
@@ -14,12 +20,26 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df = get_dataframe(station)
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    df = get_dataframe(station)
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    df = get_dataframe(station)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
